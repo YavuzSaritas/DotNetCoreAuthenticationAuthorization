a768224a29b3af80d385e7c22ac4f33822e0b56f
yavuz.saritas <yavuz.saritas@demirorenteknoloji.com>
2020-10-29 14:49:51 +0300

    Authorization-Ep.4.2 /  OperationAuthorizationRequirement - authorize operations (securing functions) & IAuthentication Index.html

---
+++ AuthenticationAuthorization/AuthenticationAuthorization/Transformer/ClaimTransformation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Basics.Transformer
+{
+    /*Dönüşüm Arayüzü*/
+    public class ClaimTransformation : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            /*Home/Authenticate() de Claimsler tanımlandıktan sonra butaya düşer*/
+            var hasFriendClaim = principal.Claims.Any(x => x.Type == "Friend");
+            if (!hasFriendClaim)
+            {
+                ((ClaimsIdentity)principal.Identity).AddClaim(new Claim("Friend", "Bad"));
+            }
+            return Task.FromResult(principal);
+        }
+    }
+}
